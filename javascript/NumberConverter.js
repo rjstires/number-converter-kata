044977ee08fc678b70fe6daa939d72e3dafb2ddb
Robert Stires <rjstires@gmail.com>
2017-10-14 13:40:51 -0400

    Got green.

--- javascript/NumberConverter.js
+++ javascript/NumberConverter.js
@@ -1,8 +1,97 @@
+const {
+  both, compose, cond,
+  equals, filter, isNil,
+  map, join, lensIndex,
+  over, reduce, reject,
+  reverse, set, splitEvery,
+  tap, toString, trim,
+  view
+} = require('ramda');
 //- Courtesy of James Grenning (@jwgrenning) and Jeff Langr (@jlangr)
 //- Use this to learn TDD
+const _ = undefined;
+const first = [_, 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+const tens = [_, _, 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+const places = [_, 'thousand', 'million']
 
-const toWords = (number) => {
-  throw new Error('First test is failing!');
+const getDigitCount = (number) => {
+  return Math.max(Math.floor(Math.log10(Math.abs(number))), 0) + 1;
 }
+
+const getDigit = (number, n, fromLeft) => {
+  const location = fromLeft ? getDigitCount(number) + 1 - n : n;
+  return Math.floor((number / Math.pow(10, location - 1)) % 10);
+}
+
+const debug = tap(console.log);
+
+// const valueLens = lensIndex(0);
+// const getValue = view(valueLens);
+// const setValue = set(valueLens);
+// const updateValue = over(valueLens);
+
+// const indexLens = lensIndex(1);
+// const getIndex = view(indexLens);
+// const setIndex = set(indexLens);
+// const updateIndex = over(indexLens);
+
+// const arrayLens = lensIndex(2);
+// const getArray = view(arrayLens);
+// const setArray = set(arrayLens);
+// const updateArray = over(arrayLens);
+
+const isZero = ([value, index, array]) => value === 0 && array.length === 1;
+const isOneToNineteen = ([value, index, array]) => value >= 1 && value < 20;
+const isTwentyToNinetyNine = ([value, index, array]) => value >= 20 && value < 100;
+const isOverOneHundred = ([value, index, array]) => value >= 100;
+
+const handleZero = ([value, index, array]) => 'zero';
+
+const handleOneToNineteen = ([value, index, array]) => first[value];
+
+const handleTwentyToNinetyNine = ([value, index, array]) => (value % 10 === 0)
+  ? tens[getDigit(value, getDigitCount(value))]
+  : `${tens[getDigit(value, 2)]}-${first[getDigit(value, 1)]}`;
+
+const handleOverOneHundred = ([value, index, array]) => (value % 100 === 0)
+  ? `${first[getDigit(value, 3)]} hundred`
+  : `${first[getDigit(value, 3)]} hundred ${toWords(parseInt(`${getDigit(value, 2)}${getDigit(value, 1)}`))}`;
+
+const handler = cond([
+  [isZero, () => 'zero'],
+  [isOneToNineteen, handleOneToNineteen],
+  [isTwentyToNinetyNine, handleTwentyToNinetyNine],
+  [isOverOneHundred, handleOverOneHundred]
+])
+
+const x = compose(
+  handler,
+  ([value, index, array]) => [parseInt(value), index, array],
+);
+
+const intoSets = compose(
+  map(reverse),
+  reverse,
+  splitEvery(3),
+  reverse
+);
+
+const addPlaces = compose(
+  reverse,
+  map(trim),
+  reject(isNil),
+  (set) => set.map((value, index) => {
+    const place = places[index];
+    return (place && value) ? `${value} ${place}` : value;
+  }),
+  reverse
+);
+const toWords = compose(
+  join(' '),
+  addPlaces,
+  (set) => set.map((val, index, array) => (val === '000') ? null : x([val, index, array])),
+  intoSets,
+  toString
+);
 
 module.exports = toWords;
--- javascript/NumberConverter.test.js
+++ javascript/NumberConverter.test.js
@@ -7,66 +7,66 @@ test('0', () => {
   expect(toWords(0)).toBe('zero');
 });
 
-test.skip('single digit (1 - 9)', () => {
+test('single digit (1 - 9)', () => {
   expect(toWords(1)).toBe('one');
   expect(toWords(9)).toBe('nine');
 });
 
-test.skip('double digit under twenty (10 - 19)', () => {
+test('double digit under twenty (10 - 19)', () => {
   expect(toWords(10)).toBe('ten');
   expect(toWords(19)).toBe('nineteen');
 });
 
-test.skip('multiples of ten (10, 50, 90)', () => {
+test('multiples of ten (10, 50, 90)', () => {
   expect(toWords(20)).toBe('twenty');
   expect(toWords(90)).toBe('ninety');
 });
 
-test.skip('not multiples of ten (21-99)', () => {
+test('not multiples of ten (21-99)', () => {
   expect(toWords(21)).toBe('twenty-one');
   expect(toWords(55)).toBe('fifty-five');
   expect(toWords(99)).toBe('ninety-nine');
 });
 
-test.skip('hundreds (100, 500, 900)', () => {
+test('hundreds (100, 500, 900)', () => {
   expect(toWords(100)).toBe('one hundred');
   expect(toWords(900)).toBe('nine hundred');
 });
 
-test.skip('not multiples of one hundred (101-999)', () => {
+test('not multiples of one hundred (101-999)', () => {
   expect(toWords(101)).toBe('one hundred one');
   expect(toWords(999)).toBe('nine hundred ninety-nine');
 });
 
-test.skip('thousands (1000, 5000, 9000)', () => {
+test('thousands (1000, 5000, 9000)', () => {
   expect(toWords(1000)).toBe('one thousand');
   expect(toWords(9000)).toBe('nine thousand');
 });
 
-test.skip('not even thousands (9999, 9911)', () => {
+test('not even thousands (9999, 9911)', () => {
   expect(toWords(9999)).toBe('nine thousand nine hundred ninety-nine');
   expect(toWords(9911)).toBe('nine thousand nine hundred eleven');
 });
 
-test.skip('tens of thousands (10,000, 19,000)', () => {
+test('tens of thousands (10,000, 19,000)', () => {
   expect(toWords(10000)).toBe('ten thousand');
   expect(toWords(19000)).toBe('nineteen thousand');
 });
 
-test.skip('tens of thousands and some (19,999)', () => {
+test('tens of thousands and some (19,999)', () => {
   expect(toWords(19999)).toBe('nineteen thousand nine hundred ninety-nine');
 });
 
-test.skip('hundred thousand (100,000, 700,000)', () => {
+test('hundred thousand (100,000, 700,000)', () => {
   expect(toWords(100000)).toBe('one hundred thousand');
   expect(toWords(700000)).toBe('seven hundred thousand');
 });
 
-test.skip('not exactly hundred thousand (198,000, 701,020)', () => {
+test('not exactly hundred thousand (198,000, 701,020)', () => {
   expect(toWords(198000)).toBe('one hundred ninety-eight thousand');
   expect(toWords(701020)).toBe('seven hundred one thousand twenty');
 });
 
-test.skip('millions (1,000,000)', () => {
+test('millions (1,000,000)', () => {
   expect(toWords(1000000)).toBe('one million');
 });
